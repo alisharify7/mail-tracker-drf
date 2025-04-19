ecedfc0072d4c929e6cdeffe7102a264bfdaf4f7
ali sharifi <alisharifyofficial@gmail.com>
2025-04-19 21:36:42 +0330

    minor changes

--- core/urls.py
+++ core/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("mail/", include("mailer.urls")),
+    path("attachment/", include("attachments.urls")),
 ]
--- mailer/urls.py
+++ mailer/urls.py
@@ -1,3 +1,12 @@
+"""
+* mail-tracker-drf
+* author: github.com/alisharify7
+* email: alisharifyofficial@gmail.com
+* license: see LICENSE for more details.
+* Copyright (c) 2025 - ali sharifi
+* https://github.com/alisharify7/mail-tracker-drf
+"""
+
 from django.urls import path
 from mailer import views
 
