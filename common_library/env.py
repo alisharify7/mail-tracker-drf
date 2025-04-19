5d22632594a59aed965e4207f75e5a2a991877bb
ali sharifi <alisharifyofficial@gmail.com>
2025-04-19 18:13:30 +0330

    add required_env utils for reading envs

---
+++ common_library/env.py
@@ -0,0 +1,12 @@
+from decouple import config, UndefinedValueError
+from django.core.exceptions import ImproperlyConfigured
+
+
+def required_env(varname, **kwargs):
+    """
+    DATABASE_URL = required_env('DATABASE_URL')
+    """
+    try:
+        return config(varname, **kwargs)
+    except UndefinedValueError:
+        raise ImproperlyConfigured(f"Environment variable {varname!r} is required but not set.")
