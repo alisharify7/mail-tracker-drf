d1ba39481a7aafc35e04153a4564b9b8d7636c66
ali sharifi <alisharifyofficial@gmail.com>
2025-04-20 21:19:50 +0330

    return attachment details for attachemnts

--- attachments/serializers.py
+++ attachments/serializers.py
@@ -9,11 +9,21 @@
 
 from rest_framework import serializers
 
-from attachments.models import Attachment
+from attachments.models import Attachment, AttachmentType
+
+
+class AttachmentTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AttachmentType
+        fields = ["main_type", "sub_type"]
 
 
 class AttachmentSerializer(serializers.ModelSerializer):
+    attachment_type_detail = AttachmentTypeSerializer(
+        source="attachment_type", read_only=True
+    )
+
     class Meta:
         model = Attachment
-        exclude = ["id"]
-        read_only_fields = ["created_time", "modified_time"]
+        exclude = ["id", "attachment_type"]
+        read_only_fields = ["created_time", "modified_time", "public_key"]
