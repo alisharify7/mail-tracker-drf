69d8b1a233e619cd8533ae2f89251a136caddb5e
ali sharifi <alisharifyofficial@gmail.com>
2025-04-21 17:31:30 +0330

    Update mongodb_models.py

--- mailer/mongodb_models.py
+++ mailer/mongodb_models.py
@@ -12,11 +12,17 @@ from mongoengine import Document, ObjectIdField, StringField, IntField, URLField
 
 class MailEvent(Document):
     """MongoDB Collection definition for Mail Events"""
+    meta = {
+        'db_alias': 'default'
+    }
 
-    _id = ObjectIdField(primary_key=True)
-    event_type = StringField(required=True, choices=["open", "link-click"])
+    LINK_CLICK = "link-click"
+    OPEN = "open"
+    event_type_choices = [OPEN, LINK_CLICK]
+
+    event_type = StringField(required=True, choices=event_type_choices)
     sql_mail_id = IntField(required=True)
-    redirect_to = URLField(required=False, null=True)
+    redirect_to = URLField(required=False)
     created_time = DateTimeField(default=lambda: datetime.datetime.now(datetime.UTC))
     modified_time = DateTimeField(default=lambda: datetime.datetime.now(datetime.UTC))
 
@@ -27,7 +33,3 @@ class MailEvent(Document):
     def _save_update(self, *args, **kwargs):
         self.modified_time = datetime.datetime.now(datetime.UTC)
         super()._save_update(*args, **kwargs)
-
-    def clean(self):
-        if self.event_type != "link-click":
-            self.redirect_to = None
